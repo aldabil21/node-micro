16417569266b0caf0c2d108bff09e27dec96e546
blbl <ali.aldabil@gmail.com>
2021-06-11 19:07:44 +0300

    fix tickets test

--- ticket/src/db/pool.ts
+++ ticket/src/db/pool.ts
@@ -1,22 +1,12 @@
-import mysql from "mysql2";
+import mysql, { PoolOptions } from "mysql2";
 
-type DBconfig = {
-  connectionLimit: number;
-  port: number;
-  host: string | undefined;
-  user: string | undefined;
-  password: string | undefined;
-  database?: string;
-  multipleStatements?: boolean;
-};
-
-const CONFIG: DBconfig = {
+const CONFIG: PoolOptions = {
   connectionLimit: 10,
   port: 3306,
-  host: process.env.DB_HOST?.toString(),
-  user: process.env.DB_USER,
-  password: process.env.ROOT_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST?.toString()!,
+  user: process.env.DB_USER!,
+  password: process.env.ROOT_PASSWORD!,
+  database: process.env.DB_NAME!,
 };
 
 const db = mysql.createPool(CONFIG).promise();
